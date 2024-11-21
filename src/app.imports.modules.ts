3f967ffba6a1475361116ac9ac45bc024b368685
moolight <binhnguyenthanh19242yahoo@gmail.com>
2024-11-21 21:28:10 +0700

    remove unused files and configurations

--- src/app.imports.modules.ts
+++
@@ -1,6 +0,0 @@
-import { TodosModule } from "src/todos/todos.module";
-import { UsersModule } from "src/users/users.module";
-
-const AppImportModules = [UsersModule, TodosModule];
-
-export { AppImportModules };
